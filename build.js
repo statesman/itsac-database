549311a017c5055b335245e4ae78d20a31c2ba47
Andrew Chavez <adchavez@gmail.com>
2015-02-27 09:36:05 -0600

    Refactor build file

--- build.js
+++ build.js
@@ -1,120 +1,110 @@
 var mysql = require('mysql'),
-    _ = require('underscore'),
     async = require('async'),
     fs = require('fs'),
     moment = require('moment');
 
-/*
- * Default connection settings
- */
-var db = {
-  host: 'waller.statesman.com',
-  user: 'reporter',
-  password: process.argv[2],
-  database: 'dir'
-};
 
-/*
- * Get a connection and run the top-level, aggregated contractor query
- */
-var connection = mysql.createConnection(db);
+var contractorsQuery = fs.readFileSync('queries/contractors.sql', 'utf8'),
+    contractorQuery = fs.readFileSync('queries/contractor.sql', 'utf8'),
+    topByAgencyQuery = fs.readFileSync('queries/top-agency-contractors.sql', 'utf8');
 
-var allRows;
-
-var contractorsQuery = fs.readFileSync('queries/contractors.sql', 'utf8');
-
-console.log("Running all-contractors query.");
-
-connection.query(contractorsQuery, function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log("Writing contractors.json file.");
-
-  rows = parseRows(rows);
-  allRows = rows;
-  fs.writeFileSync('public/data/contractors.json', JSON.stringify(rows));
-  buldIndividualFiles(rows);
+async.waterfall([
+  dbConnect,
+  contractorCollection,
+  contractorModels
+], function(err, results) {
+  if(err) throw err;
 });
 
-connection.end();
+// Connect to the DB
+function dbConnect(cb) {
+  var pool = mysql.createPool({
+    host: 'waller.statesman.com',
+    user: 'reporter',
+    password: process.argv[2],
+    database: 'dir',
+    connectionLimit: 10
+  });
+  cb(null, pool);
+}
 
-// Parse the rows before saving them as JSON
-function parseRows(rows) {
-  return rows.map(function(row, i) {
-    row.i = i;
-    row.searchable = row.name.split(' ');
-    row.id = buildSlug(row.name, i);
-    return row;
+// Get a list of all contractors, then loop through that query to build individual contractor files
+function contractorCollection(pool, cb) {
+  console.log("Running all-contractors query.");
+  pool.query(contractorsQuery, function(err, rows, fields) {
+    if (err) return cb(err);
+
+    console.log("Writing contractors.json file.");
+
+    rows = parseRows(rows);
+    fs.writeFileSync('public/data/contractors.json', JSON.stringify(rows));
+    cb(null, pool, rows);
   });
 }
 
-// Build a slug that can be used in the URL
-function buildSlug(name, i) {
-  return i.toString() + '.' + name.toLowerCase().replace(/\W/g, '');
-}
-
-/*
- * Now setup a pool of connections to simultaneously run all of the individual
- * contractor page queries
- */
-var pool = mysql.createPool(_.extend(db, {
-  connectionLimit: 10
-}));
-
-function buldIndividualFiles(rows) {
+// Build the individual files
+function contractorModels(pool, rows, cb) {
   console.log("Building individual contractor files.");
 
-  async.eachLimit(rows, 10, buildContractorFile, function(err) {
-    if(err) throw err;
+  async.eachLimit(rows, 10, buildContractorFile.bind({
+    pool: pool
+  }), function(err) {
+    if(err) return cb(err);
 
     pool.end();
+    cb(null);
   });
 }
 
-var contractorQuery = fs.readFileSync('queries/contractor.sql', 'utf8');
-var topByAgencyQuery = fs.readFileSync('queries/top-agency-contractors.sql', 'utf8');
+
+/*
+ * File builders for individual model files
+ */
 
 // Build an individual contractor file
 function buildContractorFile(row, cb) {
   console.log("Building file for", row.name);
 
-  pool.getConnection(function(err, connection) {
+  this.pool.getConnection(function(err, connection) {
     if (err) return cb(err);
 
     connection.query(contractorQuery, [row.name, row.vendor, row.agency], function(err, transactions) {
+      connection.release();
+
       if(err) return cb(err);
 
-      connection.query(topByAgencyQuery, [row.agency], function(err, topAgency) {
+      // Add the ID to the JSON file
+      row.transactions = transactions.map(function(transaction) {
+        transaction.month = parseDate(transaction.month);
+        return transaction;
+      });
 
-        connection.release();
-
-        if(err) return cb(err);
-
-        // Add the ID to the JSON file
-        row.transactions = transactions.map(function(transaction) {
-          transaction.month = parseDate(transaction.month);
-          return transaction;
-        });
-
-        row.topAgency = topAgency.map(function(top) {
-          var related = _.findWhere(allRows, {name: top.name, agency: row.agency});
-          top.id = related.id;
-          if(row.name === top.name && row.vendor === related.vendor) {
-            top.same = true;
-          }
-          return top;
-        });
-
-        fs.writeFile('public/data/contractors/' + row.i + '.json', JSON.stringify(row), function (err) {
-          if (err) return cb(err);
-
-          cb();
-        });
-
-
+      fs.writeFile('public/data/contractors/' + row.i + '.json', JSON.stringify(row), function (err) {
+        if (err) return cb(err);
+        cb();
       });
     });
   });
+}
+
+
+/*
+ * Helper functions
+ */
+
+// Parse the rows before saving them as JSON
+function parseRows(rows) {
+ return rows.map(function(row, i) {
+   row.i = i;
+   row.searchable = row.name.split(' ');
+   row.id = buildSlug(row.name, i);
+   return row;
+ });
+}
+
+// Build a slug that can be used in the URL
+function buildSlug(name, i) {
+ return i.toString() + '.' + name.toLowerCase().replace(/\W/g, '');
 }
 
 // A date parser that turns SQL dates into ISO8601 for JavaScript
